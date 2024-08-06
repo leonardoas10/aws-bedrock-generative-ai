cf90171797f1e5cd17511501c7d0f958cb671d89
Leonardo Aranguren S <leoaranguren10@gmail.com>
2024-08-05 20:03:38 -0400

    Learning use RAG with PDF's

---
+++ src/langchain/PdfRag.ts
@@ -0,0 +1,65 @@
+import { BedrockEmbeddings } from '@langchain/aws';
+import { Bedrock } from '@langchain/community/llms/bedrock';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { ChatPromptTemplate } from '@langchain/core/prompts';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+
+const AWS_REGION = 'us-east-1';
+
+const model = new Bedrock({
+    model: 'amazon.titan-text-express-v1',
+    region: AWS_REGION,
+});
+
+const question = 'What themes does Gone with the Wind explore?';
+
+async function main() {
+    const loader = new PDFLoader('assets/books.pdf', {
+        splitPages: false,
+    });
+
+    const docs = await loader.load();
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        separators: [`, \n`],
+    });
+
+    const splittedDocs = await splitter.splitDocuments(docs);
+
+    const vectorStore = new MemoryVectorStore(
+        new BedrockEmbeddings({
+            region: AWS_REGION,
+        })
+    );
+
+    await vectorStore.addDocuments(splittedDocs);
+
+    const retriever = vectorStore.asRetriever({
+        k: 2,
+    });
+
+    const results = await retriever.invoke(question);
+
+    const resultDocs = results.map((result) => result.pageContent);
+
+    //build template:
+    const template = ChatPromptTemplate.fromMessages([
+        [
+            'system',
+            'Based on the following context: {context}, answer the questions accurately without adding any additional information. ',
+        ],
+        ['user', '{input}'],
+    ]);
+
+    const chain = template.pipe(model);
+
+    const response = await chain.invoke({
+        input: question,
+        context: resultDocs,
+    });
+
+    console.log(response);
+}
+
+main();
