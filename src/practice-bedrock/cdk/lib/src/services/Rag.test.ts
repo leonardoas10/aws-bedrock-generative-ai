8991c709674698afda6bdee9e5bbc855a8df6ed3
Leonardo Aranguren S <leoaranguren10@gmail.com>
2024-08-17 22:03:11 -0400

    Adding Rag.ts and test

---
+++ src/practice-bedrock/cdk/lib/src/services/Rag.test.ts
@@ -0,0 +1,13 @@
+import { handler } from './Rag';
+
+async function testRag() {
+    const result = await handler(
+        {
+            body: JSON.stringify({ question: 'What does GDPR stand for?' }),
+        } as any,
+        {} as any
+    );
+    console.log(result);
+}
+
+testRag();
---
+++ src/practice-bedrock/cdk/lib/src/services/Rag.ts
@@ -0,0 +1,50 @@
+import {
+    APIGatewayProxyEvent,
+    Context,
+    APIGatewayProxyResult,
+} from 'aws-lambda';
+import {
+    BedrockAgentRuntimeClient,
+    RetrieveAndGenerateCommand,
+} from '@aws-sdk/client-bedrock-agent-runtime';
+
+const AWS_REGION_BEDROCK = 'us-east-1';
+
+const client = new BedrockAgentRuntimeClient({ region: AWS_REGION_BEDROCK });
+
+export async function handler(
+    event: APIGatewayProxyEvent,
+    context: Context
+): Promise<APIGatewayProxyResult> {
+    if (event.body) {
+        const body = JSON.parse(event.body);
+        const question = body.question as string;
+        if (question) {
+            const command = new RetrieveAndGenerateCommand({
+                input: {
+                    text: question,
+                },
+                retrieveAndGenerateConfiguration: {
+                    type: 'KNOWLEDGE_BASE',
+                    knowledgeBaseConfiguration: {
+                        knowledgeBaseId: 'PSX4JIJMQN',
+                        modelArn: `arn:aws:bedrock:${AWS_REGION_BEDROCK}::foundation-model/anthropic.claude-v2`,
+                    },
+                },
+            });
+            const response = await client.send(command);
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    response: response.output?.text,
+                }),
+            };
+        }
+    }
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            error: 'Bad request',
+        }),
+    };
+}
